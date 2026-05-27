a04514a4055ffb3d49d79882cc9f2c77f29b9ccb
San Mes <serhii@sanmes.app>
2026-05-28 01:22:30 +0200

    fix: restore correct dependency versions for Expo 54, bundle works

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Force Babel transformation on packages that use private class fields
+// (Hermes doesn't support them natively)
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
 module.exports = config;
